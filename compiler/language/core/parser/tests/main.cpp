d9a9a76eb24a3162ec34b0f75cf9caee28654af1
Jeff Panici <jeff.panici@panici-software.com>
2019-08-01 07:17:06 -0600

    language: core parser shell

---
+++ compiler/language/core/parser/tests/main.cpp
@@ -0,0 +1,25 @@
+// ----------------------------------------------------------------------------
+// ____                               _
+// |  _\                             | |
+// | |_)| __ _ ___  ___  ___ ___   __| | ___ TM
+// |  _< / _` / __|/ _ \/ __/ _ \ / _` |/ _ \
+// | |_)| (_| \__ \  __/ (_| (_) | (_| |  __/
+// |____/\__,_|___/\___|\___\___/ \__,_|\___|
+//
+//       C O M P I L E R  P R O J E C T
+//
+// Copyright (C) 2019 Jeff Panici
+// All rights reserved.
+//
+// This software source file is licensed under the terms of MIT license.
+// For details, please read the LICENSE file.
+//
+// ----------------------------------------------------------------------------
+
+#define CATCH_CONFIG_RUNNER
+#include <catch2/catch.hpp>
+
+int main(int argc, char** argv) {
+    auto result = Catch::Session().run(argc, argv);
+    return (result < 0xff ? result : 0xff);
+}
---
+++ compiler/language/core/parser/tests/parser_tests.cpp
@@ -0,0 +1,30 @@
+// ----------------------------------------------------------------------------
+// ____                               _
+// |  _\                             | |
+// | |_)| __ _ ___  ___  ___ ___   __| | ___ TM
+// |  _< / _` / __|/ _ \/ __/ _ \ / _` |/ _ \
+// | |_)| (_| \__ \  __/ (_| (_) | (_| |  __/
+// |____/\__,_|___/\___|\___\___/ \__,_|\___|
+//
+//       C O M P I L E R  P R O J E C T
+//
+// Copyright (C) 2019 Jeff Panici
+// All rights reserved.
+//
+// This software source file is licensed under the terms of MIT license.
+// For details, please read the LICENSE file.
+//
+// ----------------------------------------------------------------------------
+
+#include <catch2/catch.hpp>
+#include <compiler/defer.h>
+#include <compiler/formatters/formatters.h>
+
+namespace basecode {
+
+    using namespace std::literals;
+    using namespace compiler;
+    using namespace compiler::language::core;
+
+
+}
